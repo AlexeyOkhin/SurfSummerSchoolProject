9c9aaba7af0c4ca4aeba3e22540305714ef36f55
AlexeyOkhin <k-i-ll-e-r@list.ru>
2022-08-09 00:36:15 +0500

    fix bug for imageView in detailModule

--- SurfSummerSchoolProject/MainFlow/DetailModule/DetailViewController.swift
+++ SurfSummerSchoolProject/MainFlow/DetailModule/DetailViewController.swift
@@ -30,10 +30,6 @@ class DetailViewController: UIViewController, UIGestureRecognizerDelegate {
         configureNavigationBar()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        tableView.reloadData()
-    }
 }
 
 //MARK: - Private Method
