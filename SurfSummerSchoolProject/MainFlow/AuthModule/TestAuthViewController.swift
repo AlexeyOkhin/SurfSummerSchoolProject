78de87d22e303e401306de64e9f633262afc260a
AlexeyOkhin <k-i-ll-e-r@list.ru>
2022-08-17 20:42:23 +0500

    configure loginButton and move content of keyboard show

--- SurfSummerSchoolProject/MainFlow/AuthModule/TestAuthViewController.swift
+++ SurfSummerSchoolProject/MainFlow/AuthModule/TestAuthViewController.swift
@@ -6,26 +6,19 @@
 //
 
 import UIKit
-
-enum TextFieldTag: Int {
-    case loginFieldTag = 0
-    case passwordFieldTag = 1
-}
-
 class TestAuthViewController: UIViewController {
     
-    
-    
+    enum TextFieldTag: Int {
+        case loginFieldTag = 0
+        case passwordFieldTag = 1
+    }
     
     //MARK: - IBUutlet constraints
     
-    @IBOutlet private weak var keybordUpConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var keyboardUpConstraint: NSLayoutConstraint!
     
-    @IBOutlet private weak var passwordLeftConstraint: NSLayoutConstraint!
     @IBOutlet private weak var passwordTopConstraint: NSLayoutConstraint!
-    
     @IBOutlet private weak var loginTopConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var loginLeftConstraint: NSLayoutConstraint!
     
     //MARK: - IBOutlets Views
     
@@ -48,16 +41,54 @@ class TestAuthViewController: UIViewController {
     
     @IBOutlet private weak var passwordSecurityButton: UIButton!
     
+    //MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         configureLoginField()
         configurePasswordField()
-       
+        configureLoginButton()
         
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: self.view.window)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: self.view.window)
+    }
+    
+    //MARK: - TouchesBegan
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //MARK: - Action Methods
+    
+    @IBAction func shadowPasswordAction(_ sender: UIButton) {
+        passwordTF.isSecureTextEntry.toggle()
+        if passwordTF.isSecureTextEntry {
+            sender.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        } else {
+            sender.setImage(UIImage(systemName: "eye"), for: .normal)
+        }
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+            if keyboardUpConstraint.constant == 55 {
+                keyboardUpConstraint.constant = 35
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if keyboardUpConstraint.constant == 35 {
+            keyboardUpConstraint.constant = 55
+        }
+    }
 }
-
-
 
 //MARK: -  Private Method
 
@@ -116,9 +147,42 @@ extension TestAuthViewController {
         
         errorPasswordLabel.font = .systemFont(ofSize: 12)
         errorPasswordLabel.textColor = Constants.Color.errorLabel
+        
+        passwordSecurityButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        passwordSecurityButton.isHidden = true
+        passwordSecurityButton.tintColor = Constants.Color.placeHolderTF
+    }
+    
+    ///Floating Label
+    func floatTitle(title: UILabel, constraint: NSLayoutConstraint) {
+        title.font = .systemFont(ofSize: 12)
+        constraint.constant = 4
+    }
+    
+    func performAnimation(transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+            self.titleLabel.transform = transform
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func unfloatTitle(title: UILabel, constraint: NSLayoutConstraint) {
+        constraint.constant = 18
+        title.font = .systemFont(ofSize: 16)
+        performAnimation(transform: CGAffineTransform(scaleX: 1, y: 1))
+    }
+    
+    /// Configure login button
+    func configureLoginButton() {
+        loginButton.backgroundColor = .black
+        loginButton.titleLabel?.textColor = .white
+        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        loginButton.setTitle("Войти", for: .normal)
+        loadingIndicatorImage.image = Constants.Image.loadingIndicator
+        loadingIndicatorImage.isHidden = true
+        
     }
 }
-
 
 //MARK: - Extensions for Delegate
 
@@ -128,52 +192,32 @@ extension TestAuthViewController: UITextFieldDelegate {
         
         switch TextFieldTag(rawValue: textField.tag) {
         case .loginFieldTag:
-            
             floatTitle(title: loginFloatingLabel, constraint: loginTopConstraint)
             performAnimation(transform: CGAffineTransform(scaleX: 1, y: 1))
         case .passwordFieldTag:
             floatTitle(title: passwordFloatingLabel, constraint: passwordTopConstraint)
             performAnimation(transform: CGAffineTransform(scaleX: 1, y: 1))
+            passwordSecurityButton.isHidden = false
         case .none:
             break
         }
     }
     
-    func floatTitle(title: UILabel, constraint: NSLayoutConstraint) {
-        title.font = .systemFont(ofSize: 12)
-        constraint.constant = 4
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        switch TextFieldTag(rawValue: textField.tag) {
+        case .loginFieldTag:
+            if textField.text?.isEmpty ?? false {
+                unfloatTitle(title: loginFloatingLabel, constraint: loginTopConstraint)
+            }
+        case .passwordFieldTag:
+            if textField.text?.isEmpty ?? false {
+                unfloatTitle(title: passwordFloatingLabel, constraint: loginTopConstraint)
+                passwordSecurityButton.isHidden = true
+            }
+        case .none:
+            break
+        }
     }
-    
-    private func performAnimation(transform: CGAffineTransform) {
-        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
-            self.titleLabel.transform = transform
-            self.view.layoutIfNeeded()
-        }, completion: nil)
-    }
-
-       func textFieldDidEndEditing(_ textField: UITextField) {
-           
-           switch TextFieldTag(rawValue: textField.tag) {
-           case .loginFieldTag:
-               
-               if textField.text?.isEmpty ?? false {
-                   unfloatTitle(title: loginFloatingLabel, constraint: loginTopConstraint)
-               }
-           case .passwordFieldTag:
-               
-               if textField.text?.isEmpty ?? false {
-                   unfloatTitle(title: passwordFloatingLabel, constraint: loginTopConstraint)
-               }
-           case .none:
-               break
-           }
-       }
-
-      func unfloatTitle(title: UILabel, constraint: NSLayoutConstraint) {
-          constraint.constant = 18
-          title.font = .systemFont(ofSize: 16)
-          performAnimation(transform: CGAffineTransform(scaleX: 1, y: 1))
-       }
-
 }
 
