31beda23d4aee0328d7117496304cbd55554eb09
AlexeyOkhin <k-i-ll-e-r@list.ru>
2022-08-20 22:12:54 +0500

    исправил отображение даты в лейблах

--- SurfSummerSchoolProject/MainFlow/MainModule/Model/DTO/DetailItemModel.swift
+++ SurfSummerSchoolProject/MainFlow/MainModule/Model/DTO/DetailItemModel.swift
@@ -27,21 +27,8 @@ struct DetailItemModel {
         self.content = content
 
         let formatter = DateFormatter()
-        formatter.dateFormat = "dd.mm.yyyy"
+        formatter.dateFormat = "dd.MM.yyyy"
 
         self.dateCreation = formatter.string(from: dateCreation)
-    }
-
-    // MARK: - Internal methods
-
-    static func createDefault() -> DetailItemModel {
-        .init(
-            id: "fsdfdsfsdfds",
-            imageUrlInString: "",
-            title: "Самый милый корги",
-            isFavorite: false,
-            content: "Для бариста и посетителей кофеен специальные кружки для кофе — это ещё один способ проконтролировать вкус напитка и приготовить его именно так, как нравится вам. \n \nТеперь, кроме регулировки экстракции, настройки помола, времени заваривания и многого что помогает выделять нужные характеристики кофе, вы сможете выбрать и кружку для кофе в зависимости от сорта.",
-            dateCreation: Date()
-        )
     }
 }
