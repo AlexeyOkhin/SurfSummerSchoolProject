baab40d08be744b53306ef4d06aebc196b047bf1
AlexeyOkhin <k-i-ll-e-r@list.ru>
2022-08-20 13:28:30 +0500

    добавил расширение для String для форматирования номера телефона для использования в профиле

---
+++ SurfSummerSchoolProject/Extensions/UIKit/UIString + Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIString + Extensions.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Djinsolobzik on 20.08.2022.
+//
+
+
+//MARK: - Extension for formater phone number for pattern  "+# (###) ###-##-##"
+
+extension String {
+    func applyPatternOnNumbers(pattern: String, replacementCharacter: Character) -> String {
+        var pureNumber = self.replacingOccurrences( of: "[^0-9]", with: "", options: .regularExpression)
+        for index in 0 ..< pattern.count {
+            guard index < pureNumber.count else { return pureNumber }
+            let stringIndex = String.Index(utf16Offset: index, in: pattern)
+            let patternCharacter = pattern[stringIndex]
+            guard patternCharacter != replacementCharacter else { continue }
+            pureNumber.insert(patternCharacter, at: stringIndex)
+        }
+        return pureNumber
+    }
+}
